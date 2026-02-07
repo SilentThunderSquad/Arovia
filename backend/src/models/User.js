e16387b189de562c114c0ddfd8ad1d5532ace968
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-02-07 21:58:40 +0530

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -63,7 +63,7 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
+  if (!this.isModified("password") || !this.password) return;
   this.password = await bcrypt.hash(this.password, 12);
 });
 
